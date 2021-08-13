0ce1fc3634c2fe4dd3679d437b7534687d94b979
Nicolas Perraud <np@bitbox.io>
2021-08-13 14:13:18 +0200

    Add benchmark on a suggestion of @yberman

--- heappermutations_test.go
+++ heappermutations_test.go
@@ -1,10 +1,11 @@
 // Copyright (c) 2021 Nicolas Perraud <np bitbox io>
+// Thanks: @yberman
 
 package heappermutations_test
 
 import (
-	"github.com/dbyio/heappermutations"
 	"fmt"
+	"github.com/dbyio/heappermutations"
 	"testing"
 )
 
@@ -18,7 +19,16 @@ func ExampleInts() {
 func TestStrings(t *testing.T) {
 	s := []string{"abc", "def", "ghi", "jkl"}
 	perms := heappermutations.Strings(s)
-	if len(perms) != 24 {		// factorial(len(s)) == 24
+	if len(perms) != 24 { // factorial(len(s)) == 24
 		t.Error("Expected 24")
 	}
+}
+
+func BenchmarkInts(b *testing.B) {
+	s := []int{}
+	for i := 0; i < 10; i++ {
+		s = append(s, i)
+	}
+	b.ResetTimer()
+	heappermutations.Ints(s)
 }
